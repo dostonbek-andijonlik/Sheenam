287e0d049275d8a333935fa8bff075eda22be163
Doston Muminov <dostonbek.andijonlik@gmail.com>
2023-03-31 07:12:15 +0500

    DATA: Migrate & Model Guests

---
+++ Sheenam.Api/Models/Foundations/Guests/Guest.cs
@@ -0,0 +1,20 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted-Engineers
+//Feel Free to Use It
+//=================================================
+namespace Sheenam.Api.Models.Foundations.Guests
+{
+    public class Guest
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string Email { get; set; }   
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+
+        public GenderType Gender { get; set; }
+
+    }
+}
